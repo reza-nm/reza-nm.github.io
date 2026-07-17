708333c1cfc3ffc76aa29cd9863f626917c533cb
reza-nm <namireza.com@gmail.com>
2026-07-17 14:15:37 +0330

    back to simple

--- src/lib/utils.js
+++
@@ -1,5 +0,0 @@
-// Tiny classname helper (shadcn/ui convention). Lets you conditionally
-// combine Tailwind classes: cn("p-4", isActive && "bg-gold")
-export function cn(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
